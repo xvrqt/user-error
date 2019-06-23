94ba4abc8802b1f69bd7f0d99da294b7e5c0beb8
Amy <git@xvrqt.com>
2019-06-23 14:21:21 -0700

    Added in lib.rs template

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+//! # User Error
+//! A library for conveniently displaying well-formatted, and good looking errors to users of CLI applications. Useful for bubbling up unrecoverable errors to inform the user what they can do to fix them.
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+// Dependencies
+use std::fmt;
+
+/// The eponymous struct.
+struct UserError {
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn test() {
         assert_eq!(2 + 2, 4);
     }
 }
